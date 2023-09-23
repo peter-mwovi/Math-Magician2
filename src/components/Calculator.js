bcfc973f1d7ce57f0172d1ba9f5d5841b4a3d025
peter-mwovi <petermmwovi@gmail.com>
2023-09-23 19:33:04 +0300

    Edit the component files

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,53 @@
-import React from 'react';
-import CalculatorDisplay from './CalculatorDisplay';
+import React, { useState } from 'react';
 import Button from './Button';
+import calculate from '../logic/calculate';
 
-export default function Calculator() {
+function Calculator() {
+  const [operation, setResult] = useState({
+    total: null,
+    second: null,
+    operation: null,
+  });
+  const [displayValue, setDisplayValue] = useState('');
+
+  const handleButtonClick = (buttonName) => {
+    const newOperation = calculate(operation, buttonName);
+    setResult(newOperation);
+    if (buttonName === 'AC') {
+      setDisplayValue('');
+    } else if (buttonName === '=') {
+      setDisplayValue(newOperation.total || '');
+    } else if (buttonName === '+/-') {
+      setDisplayValue(displayValue + buttonName);
+    } else {
+      setDisplayValue(displayValue + buttonName);
+    }
+  };
+
   return (
-    <div className="calciGrid">
-      <CalculatorDisplay />
-      <Button text="AC" styling="key1" />
-      <Button text="+/-" styling="key1" />
-      <Button text="%" styling="key1" />
-      <Button text="÷" styling="key1b" />
-      <Button text="7" styling="key1" />
-      <Button text="8" styling="key1" />
-      <Button text="9" styling="key1" />
-      <Button text="x" styling="key1b" />
-      <Button text="4" styling="key1" />
-      <Button text="5" styling="key1" />
-      <Button text="6" styling="key1" />
-      <Button text="-" styling="key1b" />
-      <Button text="1" styling="key1" />
-      <Button text="2" styling="key1" />
-      <Button text="3" styling="key1" />
-      <Button text="+" styling="key1b" />
-      <Button text="0" styling="keys2" />
-      <Button text="." styling="key1" />
-      <Button text="=" styling="key1b" />
+    <div className="calc">
+      <input className="keys4" value={displayValue} readOnly />
+      <Button text="AC" styling="key1" onClick={handleButtonClick} />
+      <Button text="+/-" styling="key1" onClick={handleButtonClick} />
+      <Button text="%" styling="key1" onClick={handleButtonClick} />
+      <Button text="÷" styling="key1b" onClick={handleButtonClick} />
+      <Button text="7" styling="key1" onClick={handleButtonClick} />
+      <Button text="8" styling="key1" onClick={handleButtonClick} />
+      <Button text="9" styling="key1" onClick={handleButtonClick} />
+      <Button text="x" styling="key1b" onClick={handleButtonClick} />
+      <Button text="4" styling="key1" onClick={handleButtonClick} />
+      <Button text="5" styling="key1" onClick={handleButtonClick} />
+      <Button text="6" styling="key1" onClick={handleButtonClick} />
+      <Button text="-" styling="key1b" onClick={handleButtonClick} />
+      <Button text="1" styling="key1" onClick={handleButtonClick} />
+      <Button text="2" styling="key1" onClick={handleButtonClick} />
+      <Button text="3" styling="key1" onClick={handleButtonClick} />
+      <Button text="+" styling="key1b" onClick={handleButtonClick} />
+      <Button text="0" styling="keys2" onClick={handleButtonClick} />
+      <Button text="." styling="key1" onClick={handleButtonClick} />
+      <Button text="=" styling="key1b" onClick={handleButtonClick} />
     </div>
   );
 }
+
+export default Calculator;
---
+++ src/components/Quotes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import RandomQuote from './RandomQuote';
+
+const Quotes = () => (
+  <div className="random">
+    <RandomQuote />
+  </div>
+);
+
+export default Quotes;
