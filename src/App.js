88160ed911c5c2b10e2404180e6529171dd0b72e
peter-mwovi <petermmwovi@gmail.com>
2023-09-23 19:34:11 +0300

    Edit the source files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import Calculator from './components/Calculator';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Calculations from './components/Calculations';
+import Quotes from './components/Quotes';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="calculations" element={<Calculations />} />
+        <Route path="quotes" element={<Quotes />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
