9c0e4e1e14afe157372184c490077820bfcddb6f
peter-mwovi <petermmwovi@gmail.com>
2023-09-23 19:32:46 +0300

    Add Navbar and Home components.

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h3>Welcome to our page!</h3>
+    <p>
+      Without mathematics, there&apos;s nothing you can do. Everything around you is
+      mathematics. Everything around you is numbers — Shakuntala Devi, Indian
+      writer and mental calculator
+    </p>
+    <p>
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      it is about understanding — William Paul Thurston, American mathematician
+    </p>
+  </div>
+);
+
+export default Home;
